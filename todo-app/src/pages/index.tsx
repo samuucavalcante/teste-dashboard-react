df072e3620506fb3629c9c2bdb39383837a080e7
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2022-05-17 14:45:27 -0300

    setting layout for component CardTodo

--- todo-app/src/pages/index.tsx
+++ todo-app/src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { PhoneIcon } from '@chakra-ui/icons'
 import {
   Box,
   Button,
@@ -6,36 +5,53 @@ import {
   Flex,
   Heading,
   Input,
-  InputGroup,
-  InputLeftElement,
   VStack
 } from '@chakra-ui/react'
+import CardTodo from 'components/CardTodo'
 
 const Index = () => {
   return (
-    <Center bgColor="primary" py={2} w="100%">
-      <VStack w="100%" px={12}>
-        <Heading color="white" fontSize="3xl">
-          Todo App
-        </Heading>
-        <Flex w="100%">
-          <Input
-            border="0"
-            borderRadius="12px 0px 0px 12px"
-            bgColor="whiteAlpha.500"
-            w="100%"
-            color="white"
-            placeholder="Procure por um todo..."
-            _placeholder={{
-              color: 'whiteAlpha.800'
-            }}
+    <Box w="100%">
+      {/* Header */}
+      <Center bgColor="primary" py={2} w="100%">
+        <VStack w="100%" px={2}>
+          <Heading color="white" fontSize="3xl">
+            Todo App
+          </Heading>
+          <Flex w="100%">
+            <Input
+              border="0"
+              borderRadius="12px 0px 0px 12px"
+              bgColor="whiteAlpha.500"
+              w="100%"
+              color="white"
+              placeholder="Procure por um todo..."
+              _placeholder={{
+                color: 'whiteAlpha.800'
+              }}
+            />
+            <Button color="primary" borderRadius="0px 12px 12px 0px">
+              Procurar
+            </Button>
+          </Flex>
+        </VStack>
+      </Center>
+
+      {/* Todo Cards */}
+      <Flex w="100%" alignItems="center" justifyContent="center">
+        <Flex flexWrap="wrap" w="100%">
+          <CardTodo
+            borderRadius="md"
+            p={4}
+            minW="350px"
+            mt={2}
+            ml={2}
+            w={['100%', '100%', '50%', '32%']}
+            boxShadow="xl"
           />
-          <Button color="primary" borderRadius="0px 12px 12px 0px">
-            Procurar
-          </Button>
         </Flex>
-      </VStack>
-    </Center>
+      </Flex>
+    </Box>
   )
 }
 
